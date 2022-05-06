5a5089ac712b820a26f87ea88ec7a502c7a6702d
malolebrin <malolebrin@gmail.com>
2022-05-06 15:52:57 +0200

    feat: add config for vitest

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config'
+import AutoImport from 'unplugin-auto-import/vite'
+
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  plugins: [
+    AutoImport({
+      imports: ['vitest'],
+      dts: true, // generate TypeScript declaration
+    }),
+  ]
+})
