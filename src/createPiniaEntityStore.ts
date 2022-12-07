14f751568ffe9910bb077b0483363310720fa553
MaloLebrin <malolebrin@gmail.com>
2022-12-07 23:23:44 +0100

    :sparkles: Create a function to create a Pinia store with the features of Pinia entity store. In order to simplify the use of the package

---
+++ src/createPiniaEntityStore.ts
@@ -0,0 +1,49 @@
+import { createActions, createGetters, createState } from './'
+import type { CreationParams, WithId } from '~/types'
+
+export function createPiniaEntityStore<T extends WithId>(args: CreationParams) {
+  const { id, options } = args
+
+  const entityStoreState = createState<T>()
+
+  let state = {
+    ...entityStoreState,
+  }
+
+  let getters = {
+    ...createGetters<T>(state),
+  }
+
+  let actions = {
+    ...createActions<T>(state),
+  }
+
+  if (options) {
+    if (options.defaultState) {
+      state = {
+        ...state,
+        ...options.defaultState,
+      }
+    }
+
+    if (options.getters) {
+      getters = {
+        ...getters,
+        ...options.getters,
+      }
+    }
+
+    if (options.actions) {
+      actions = {
+        ...actions,
+        ...options.actions,
+      }
+    }
+  }
+
+  return defineStore(id, {
+    state: () => ({ ...state }),
+    getters,
+    actions,
+  })
+}
