0dc5f18cad4b327a6430167ea6e54aaa132f072c
MaloLebrin <malolebrin@gmail.com>
2022-12-07 17:17:20 +0100

    :construction: typing options for plugins

---
+++ src/plugin.ts
@@ -0,0 +1,22 @@
+import 'pinia'
+
+// declare module 'pinia' {
+//   export interface PiniaCustomProperties {
+//     // by using a setter we can allow both strings and refs
+//     set hello(value: string | Ref<string>)
+//     get hello(): string
+
+//     // you can define simpler values too
+//     simpleNumber: number
+//   }
+// }
+
+declare module 'pinia' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  export interface DefineStoreOptionsBase<S extends StateTree, Store> {
+    // allow defining a number of ms for any of the actions
+    entityStoreOptions?: {
+      isEntityStore: boolean
+    }
+  }
+}
