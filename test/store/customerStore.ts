54903169f2f9f5ea55b699bb0309490f6deb02cb
MaloLebrin <malolebrin@gmail.com>
2022-12-07 23:25:25 +0100

    :construction: test new feature add new store with createPiniaStore function

---
+++ test/store/customerStore.ts
@@ -0,0 +1,12 @@
+import type { UserEntity } from './Store'
+import { createPiniaEntityStore } from '.../../src/createPiniaEntityStore'
+
+export const useCustomerStore = createPiniaEntityStore<UserEntity>({
+  id: 'customer',
+  options: {
+    actions: {
+    },
+  },
+})
+
+export default useCustomerStore
